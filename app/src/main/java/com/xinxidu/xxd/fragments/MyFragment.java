26555ac37fdef587b00a690f0bae6c5443f2977c
limingquan6666 <13615680696@139.com>
2016-09-06 13:04:21 +0800

    1.add myFragment title

--- app/src/main/java/com/xinxidu/xxd/fragments/MyFragment.java
+++ app/src/main/java/com/xinxidu/xxd/fragments/MyFragment.java
@@ -6,19 +6,69 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.xinxidu.xxd.R;
+import com.xinxidu.xxd.view.CircleImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by limingquan on 2016/9/1.
  */
 public class MyFragment extends Fragment {
+    @BindView(R.id.back)
+    RelativeLayout back;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_title_ok)
+    TextView tvTitleOk;
+    @BindView(R.id.ok)
+    RelativeLayout ok;
+    @BindView(R.id.base_title_layout)
+    RelativeLayout baseTitleLayout;
+    @BindView(R.id.iv_avatar)
+    CircleImageView ivAvatar;
+    @BindView(R.id.tv_name)
+    TextView tvName;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.rl_user_center)
+    RelativeLayout rlUserCenter;
+    @BindView(R.id.title_back)
+    ImageView titleBack;
+    @BindView(R.id.title_name)
+    TextView titleName;
+    @BindView(R.id.imageView5)
+    ImageView imageView5;
+    @BindView(R.id.my_collect)
+    ImageView myCollect;
+    @BindView(R.id.title_more)
+    ImageView titleMore;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.imageView2)
+    ImageView imageView2;
+    @BindView(R.id.imageView3)
+    ImageView imageView3;
+    @BindView(R.id.imageView4)
+    ImageView imageView4;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, container, false);
+        tvTitle.setText("我的");
+        ButterKnife.bind(this, view);
         return view;
     }
+
     //重写setMenuVisibility方法，不然会出现叠层的现象
     @Override
     public void setMenuVisibility(boolean menuVisibile) {
