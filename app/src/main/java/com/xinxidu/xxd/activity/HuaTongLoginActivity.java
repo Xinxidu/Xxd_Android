adc3abfe41e4cf8ec68509a87d37198491d591a9
limingquan6666 <13615680696@139.com>
2016-09-09 15:53:17 +0800

    revise trade and add login

--- app/src/main/java/com/xinxidu/xxd/activity/HuaTongLoginActivity.java
+++ app/src/main/java/com/xinxidu/xxd/activity/HuaTongLoginActivity.java
@@ -3,6 +3,7 @@ package com.xinxidu.xxd.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xinxidu.xxd.R;
 
@@ -56,6 +58,9 @@ public class HuaTongLoginActivity extends Activity {
     TextView tvLoginCommit;
     @BindView(R.id.tv_open_account)
     TextView tvOpenAccount;
+    private SharedPreferences sp;
+    private String etLoginUserValue;
+    private String etLoginPassValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,9 +68,28 @@ public class HuaTongLoginActivity extends Activity {
         setContentView(R.layout.hua_tong_lagin_activity);
         ButterKnife.bind(this);
         tvTitle.setText("登录");
+        sp = this.getSharedPreferences("userInfo", Context.MODE_WORLD_READABLE);
+        //判断记住密码多选框的状态
+        if(sp.getBoolean("ISCHECK", false))
+        {
+            //设置默认是记录密码状态
+            cbMima.setChecked(true);
+            etLoginUser.setText(sp.getString("USER_NAME", ""));
+            etLoginPass.setText(sp.getString("PASSWORD", ""));
+            //判断自动登陆多选框状态
+            if(sp.getBoolean("AUTO_ISCHECK", false))
+            {
+                //设置默认是自动登录状态
+                cbAuto.setChecked(true);
+                //跳转界面
+                Intent intent = new Intent(HuaTongLoginActivity.this,HotActivity.class);
+                startActivity(intent);
+
+            }
+        }
     }
 
-    @OnClick({R.id.back, R.id.tv_for_verification_code, R.id.tv_login_commit, R.id.tv_open_account})
+    @OnClick({R.id.back, R.id.tv_for_verification_code, R.id.tv_login_commit, R.id.tv_open_account,R.id.cb_mima,R.id.cb_auto})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.back:
@@ -74,10 +98,52 @@ public class HuaTongLoginActivity extends Activity {
             case R.id.tv_for_verification_code:
                 break;
             case R.id.tv_login_commit:
+                login();
                 break;
             case R.id.tv_open_account:
                 HuaTongRegisterBasicActivity.startHuaTongRegisterBasicActivity(this);
                 break;
+            case R.id.cb_mima:
+                if (cbMima.isChecked()) {
+                    sp.edit().putBoolean("ISCHECK", true).commit();
+                }else {
+                    sp.edit().putBoolean("ISCHECK", false).commit();
+                }
+                break;
+            case R.id.cb_auto:
+                if (cbAuto.isChecked()) {
+                    sp.edit().putBoolean("AUTO_ISCHECK", true).commit();
+                } else {
+                    sp.edit().putBoolean("AUTO_ISCHECK", false).commit();
+                }
+                break;
+        }
+    }
+
+    private void login() {
+        etLoginUserValue = etLoginUser.getText().toString();
+        etLoginPassValue = etLoginPass.getText().toString();
+
+        if (etLoginUserValue.equals("xxd") && etLoginPassValue.equals("123")) {
+
+            //登录成功和记住密码框为选中状态才保存用户信息
+            if(cbMima.isChecked())
+            {
+                //记住用户名、密码、
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("USER_NAME", etLoginUserValue);
+                editor.putString("PASSWORD",etLoginPassValue);
+                editor.commit();
+            }
+
+            //跳转界面
+            Intent intent = new Intent(HuaTongLoginActivity.this, HotActivity.class);
+            startActivity(intent);
+            Toast.makeText(HuaTongLoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Toast.makeText(HuaTongLoginActivity.this, "用户名或密码错误，请重新登录", Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/xinxidu/xxd/fragments/CycBuyFragment.java
+++ app/src/main/java/com/xinxidu/xxd/fragments/CycBuyFragment.java
@@ -1,5 +1,7 @@
 package com.xinxidu.xxd.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xinxidu.xxd.R;
 
@@ -18,7 +21,7 @@ import butterknife.OnClick;
 
 /**
  * Created by limingquan on 2016/9/8.
- * 交易周期买
+ * 交易  周期卖
  */
 public class CycBuyFragment extends Fragment {
     @BindView(R.id.bt_name)
@@ -39,12 +42,6 @@ public class CycBuyFragment extends Fragment {
     Button btCollect;
     @BindView(R.id.relative)
     RelativeLayout relative;
-    @BindView(R.id.bt_jian)
-    TextView btJian;
-    @BindView(R.id.tv_price_num)
-    Button tvPriceNum;
-    @BindView(R.id.bt_jia)
-    TextView btJia;
     @BindView(R.id.relative1)
     RelativeLayout relative1;
     @BindView(R.id.textView4)
@@ -53,44 +50,128 @@ public class CycBuyFragment extends Fragment {
     TextView tvPriceSell;
     @BindView(R.id.tv_num_sell)
     TextView tvNumSell;
+    @BindView(R.id.tv_tv_trade_puy_sell)
+    TextView tvTvTradePuySell;
+    @BindView(R.id.bt_jian)
+    TextView btJian;
+    @BindView(R.id.tv_price_num)
+    TextView tvPriceNum;
+    @BindView(R.id.bt_jia)
+    TextView btJia;
     @BindView(R.id.bt_jian1)
     TextView btJian1;
     @BindView(R.id.tv_price_num1)
-    Button tvPriceNum1;
+    TextView tvPriceNum1;
     @BindView(R.id.bt_jia1)
     TextView btJia1;
-    @BindView(R.id.tv_tv_trade_puy_sell)
-    TextView tvTvTradePuySell;
+    public View view;
+    int num = 1000;//数量
+    int num1 = 10;//数量
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.base_trade, container, false);
+        view = inflater.inflate(R.layout.base_trade, container, false);
         ButterKnife.bind(this, view);
         relative1.setVisibility(View.GONE);
         tvTvTradePuySell.setText("周期买");
+        tvPriceNum.setInputType(android.text.InputType.TYPE_CLASS_NUMBER);
+        tvPriceNum.setText(String.valueOf(num));
+        tvPriceNum1.setInputType(android.text.InputType.TYPE_CLASS_NUMBER);
+        tvPriceNum1.setText(String.valueOf(num1));
+        btJian.setTag("+");
+        btJia.setTag("-");
+        btJian1.setTag("+");
+        btJia1.setTag("-");
         return view;
     }
 
-    @OnClick({R.id.bt_name, R.id.relative, R.id.bt_jian, R.id.bt_jia, R.id.relative1, R.id.bt_jian1, R.id.bt_jia1, R.id.tv_tv_trade_puy_sell})
+    @OnClick({R.id.bt_name, R.id.bt_jian, R.id.bt_jia, R.id.bt_jian1, R.id.bt_jia1, R.id.tv_tv_trade_puy_sell})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_name:
-                break;
-            case R.id.relative:
+                showDialog();
                 break;
             case R.id.bt_jian:
+                jian();
                 break;
             case R.id.bt_jia:
-                break;
-            case R.id.relative1:
+                jia();
                 break;
             case R.id.bt_jian1:
+                jian1();
                 break;
             case R.id.bt_jia1:
+                jia1();
                 break;
             case R.id.tv_tv_trade_puy_sell:
+//                buyDialog();
                 break;
+        }
+    }
+
+//    private void buyDialog() {
+//
+//    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("请选择类型：");
+        final String[] cities = {"白银升贴水1000", "龙天勇银", "白银现货排期", "白银基差1000"};
+        builder.setItems(cities, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                btName.setText(" " + cities[which]);
+            }
+        });
+        builder.show();
+    }
+
+    public void jian() {
+        if (btJian.getTag().equals("+")) {
+            if (--num < 0) { //先减，再判断
+                num++;
+                Toast.makeText(getActivity(), "不能小于0",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                tvPriceNum.setText(String.valueOf(num));
+
+            }
+        }
+    }
+
+    public void jia() {
+        if (btJia.getTag().equals("-")) {
+            if (++num < 0) {  //先加，再判断
+                num--;
+                Toast.makeText(getActivity(), "不能小于0", Toast.LENGTH_SHORT).show();
+            } else {
+                tvPriceNum.setText(String.valueOf(num));
+            }
+        }
+    }
+
+    public void jian1() {
+        if (btJian1.getTag().equals("+")) {
+            if (--num1 < 0) { //先减，再判断
+                num1++;
+                Toast.makeText(getActivity(), "不能小于0",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                tvPriceNum1.setText(String.valueOf(num1));
+
+            }
+        }
+    }
+
+    public void jia1() {
+        if (btJia1.getTag().equals("-")) {
+            if (++num1 < 0) {  //先加，再判断
+                num1--;
+                Toast.makeText(getActivity(), "不能小于0", Toast.LENGTH_SHORT).show();
+            } else {
+                tvPriceNum1.setText(String.valueOf(num1));
+            }
         }
     }
 }
