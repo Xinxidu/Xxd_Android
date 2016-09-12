9c39c9e16cf58be6c7e2b1efe7ee42705534c6f1
Aaron0619 <318026323@qq.com>
2016-09-12 10:19:13 +0800

    9.12

--- app/src/main/java/com/xinxidu/xxd/activity/AtionMessageActivity.java
+++ app/src/main/java/com/xinxidu/xxd/activity/AtionMessageActivity.java
@@ -4,20 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.xinxidu.xxd.R;
+import com.xinxidu.xxd.adapter.ActionMessageAdapter;
+import com.xinxidu.xxd.adapter.ProfitSkillAdapter;
+import com.xinxidu.xxd.bean.ActionMessageEvent;
+import com.xinxidu.xxd.bean.ProfitSkillEvent;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class AtionMessageActivity extends AppCompatActivity {
+    ActionMessageAdapter mActionMessageAdapter;
     @BindView(R.id.back)
     RelativeLayout back;
     @BindView(R.id.tv_title)
     TextView tvTitle;
+    private ArrayList<ActionMessageEvent> mItem;
+    private RecyclerView mRecyclerView;
 
     public static void startAtionMessageActivity(Context context) {
         Intent intent = new Intent(context, AtionMessageActivity.class);
@@ -30,6 +42,17 @@ public class AtionMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ation_message);
         ButterKnife.bind(this);
         tvTitle.setText("活动消息");
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mItem = new ArrayList<ActionMessageEvent>();
+        mItem.add(null);
+//        if (mItem != null && mItem.size() > 0) {
+
+        mActionMessageAdapter = new ActionMessageAdapter(this);
+        mRecyclerView.setAdapter(mActionMessageAdapter);
+        mActionMessageAdapter.setData(mItem);
+        mActionMessageAdapter.notifyDataSetChanged();
     }
 
     @OnClick(R.id.back)
---
+++ app/src/main/java/com/xinxidu/xxd/adapter/ActionMessageAdapter.java
@@ -0,0 +1,74 @@
+package com.xinxidu.xxd.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xinxidu.xxd.R;
+import com.xinxidu.xxd.bean.ActionMessageEvent;
+import com.xinxidu.xxd.bean.ProfitSkillEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by limingquan on 2016/9/5.
+ */
+public class ActionMessageAdapter extends RecyclerView.Adapter<ActionMessageAdapter.ViewHolder> {
+    private final Context mContext;
+
+
+    private List<ActionMessageEvent> mItemList = new ArrayList<ActionMessageEvent>();
+
+    public ActionMessageAdapter(Context mContext, List<ActionMessageEvent> itemList, Context context) {
+        mItemList = itemList;
+        this.mContext = context;
+    }
+
+    public void setData(ArrayList<ActionMessageEvent> timeList) {
+        //mItemList.clear();
+        mItemList.addAll(timeList);
+    }
+
+    public ActionMessageAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.action_message_item, parent, false);
+        ViewHolder mViewHolder = new ViewHolder(view);
+        return mViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                TaskDetailsActivity.startTaskDetailsActivity(mContext);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        View itemView;
+
+        ViewHolder(View view) {
+            super(view);
+            this.itemView = view;
+            ButterKnife.bind(this, view);
+        }
+    }
+}
