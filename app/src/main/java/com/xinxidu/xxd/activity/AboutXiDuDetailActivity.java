0aa243152dda31e8d82fdbe9811071cdcd40e35c
limingquan6666 <13615680696@139.com>
2016-09-14 16:47:29 +0800

    1.add gson xiduNews   URl

--- app/src/main/java/com/xinxidu/xxd/activity/AboutXiDuDetailActivity.java
+++ app/src/main/java/com/xinxidu/xxd/activity/AboutXiDuDetailActivity.java
@@ -4,23 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.webkit.WebView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.webkit.WebViewClient;
 
+import com.google.gson.Gson;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.xinxidu.xxd.R;
+import com.xinxidu.xxd.models.JsonData;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 
 import butterknife.BindView;
@@ -41,6 +46,8 @@ public class AboutXiDuDetailActivity extends AppCompatActivity {
     @BindView(R.id.xidu_webView)
     WebView xiduWebView;
     private String URL;
+    protected static final String HOST = "http://175.102.13.51:8080/XDSY/ZhuBan?type=.guanwang&defference=jiaoyi&indexPage=0";
+
     public static void startAboutXiDuDetailActivity(Context context) {
         Intent intent = new Intent(context, AboutXiDuDetailActivity.class);
         context.startActivity(intent);
@@ -53,53 +60,48 @@ public class AboutXiDuDetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         tvTitle.setText("西都新闻详情");
         webrequest();
-        initUI();
     }
 
     private void webrequest() {
         //创建okHttpClient对象
         OkHttpClient mOkHttpClient = new OkHttpClient();
-//创建一个Request
+        //创建一个Request
         final Request request = new Request.Builder()
-                .url("http://175.102.13.51:8080/XDSY/ZhuBan?type=.guanwang&defference=gongsi&indexPage=0\n")
+                .url(HOST)
                 .build();
-//new call
+        //new call
         Call call = mOkHttpClient.newCall(request);
-//请求加入调度
-        call.enqueue(new Callback()
-        {
+        //请求加入调度
+        call.enqueue(new Callback() {
             @Override
-            public void onFailure(Request request, IOException e)
-            {
-                Log.v("ffffff","5555555");
+            public void onFailure(Request request, IOException e) {
+                Log.v("ffffff", "5555555");
             }
 
             @Override
-            public void onResponse(final Response response) throws IOException
-            {
+            public void onResponse(final Response response) throws IOException {
                 final String res = response.body().string();
-                runOnUiThread(new Runnable()
-                {
+                runOnUiThread(new Runnable() {
                     @Override
-                    public void run()
-                    {
-                        Log.v("sucess",res);
+                    public void run() {
+                        Log.v("sucess", res);
 
                         try {
-                            JSONObject jsonObject = new JSONObject(res);
-                            int resultCode=jsonObject.getInt("flag");
-                            if (resultCode==1){
-
-                                JSONObject data=jsonObject.getJSONObject("data");
-                                JSONObject ss=data.getJSONObject("gongsi");
-                                URL=ss.toString();
-                                Log.v("1111",ss.toString());
-
-                            }else {
-                                Log.v("fail","false");
+                            JsonData dataBean = new Gson().fromJson(res,JsonData.class);
+                            if (!TextUtils.isEmpty(dataBean.getData().getJiaoyi())){
+                                String string = null;
+                                try {
+                                    string = URLDecoder.decode(dataBean.getData().getJiaoyi(),"utf-8");
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                }
+                                xiduWebView.loadDataWithBaseURL(null,dataBean.getData().getJiaoyi(),"text/html","utf-8",null);
                             }
-                        }catch (Exception e){
-                            Log.v("exception",e.toString());
+                        else {
+                                Log.v("fail", "false");
+                            }
+                        } catch (Exception e) {
+                            Log.v("exception", e.toString());
                         }
                     }
 
@@ -112,22 +114,5 @@ public class AboutXiDuDetailActivity extends AppCompatActivity {
     @OnClick(R.id.back)
     public void onClick() {
         finish();
-    }
-
-    private void initUI() {
-//        Log.v("55555",URL);
-        //WebView加载web资源
-        //xiduWebView.loadUrl("www.baidu.com");
-        xiduWebView.loadData(URL,"text/html", "UTF-8");
-        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
-        xiduWebView.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                // TODO Auto-generated method stub
-                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
-                view.loadUrl(url);
-                return true;
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/xinxidu/xxd/models/JsonData.java
@@ -0,0 +1,45 @@
+package com.xinxidu.xxd.models;
+
+public class JsonData {
+
+    private int flag;
+
+    private DataBean data;
+    private String msg;
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public static class DataBean {
+        private String jiaoyi;
+
+        public String getJiaoyi() {
+            return jiaoyi;
+        }
+
+        public void setJiaoyi(String jiaoyi) {
+            this.jiaoyi = jiaoyi;
+        }
+    }
+}
